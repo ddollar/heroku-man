8994abef37228dbd1ca7fe3109920b77b47d136d
David Dollar <ddollar@gmail.com>
2011-12-22 09:01:12 -0500

    add -k for search

--- init.rb
+++ init.rb
@@ -1,6 +1,7 @@
 $:.unshift File.expand_path("../vendor/ronn/lib", __FILE__)
 
 begin
+  require "heroku/command"
   require "rest_client"
   require "ronn/document"
   require "tmpdir"
@@ -15,8 +16,17 @@ class Heroku::Command::Man < Heroku::Command::Base
   #
   # get devcenter documentation on a TOPIC
   #
+  # -k, --keyword KEYWORD  # search the devcenter for KEYWORD
+  #
   def index
-    raise CommandFailed, "usage: heroku man TOPIC" unless topic = args.first
+    if topic = options[:keyword]
+      display "Articles matching #{topic}:"
+      display
+      search(options[:keyword])
+      return
+    end
+
+    raise Heroku::Command::CommandFailed, "usage: heroku man TOPIC" unless topic = args.first
 
     response = devcenter["/articles/#{topic}.md"].get
     article = response.to_s
@@ -52,20 +62,24 @@ class Heroku::Command::Man < Heroku::Command::Base
       end
     end
   rescue RestClient::ResourceNotFound
-    results = json_decode(devcenter["/articles.json?q=#{topic}"].get.to_s)["devcenter"]
-    results.reject! { |r| r["article"]["slug"].length > 20 }
     display "No article found named #{topic}. Perhaps try one of these?"
     display
-    longest = results.map { |r| r["article"]["slug"].length }.sort.last
-    results.sort_by { |r| r["article"]["slug"] }.each do |result|
-      display "%-#{longest}s  # %s" % [ result["article"]["slug"], result["article"]["title"] ]
-    end
+    search(topic)
   end
 
 private
 
   def devcenter
     RestClient::Resource.new("http://devcenter.heroku.com")
+  end
+
+  def search(topic)
+    results = json_decode(devcenter["/articles.json?q=#{topic}"].get.to_s)["devcenter"]
+    results.reject! { |r| r["article"]["slug"].length > 20 }
+    longest = results.map { |r| r["article"]["slug"].length }.sort.last
+    results.sort_by { |r| r["article"]["slug"] }.each do |result|
+      display "%-#{longest}s  # %s" % [ result["article"]["slug"], result["article"]["title"] ]
+    end
   end
 
 end
